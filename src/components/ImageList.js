688a86c28ed4577b94e255ede02c7556af6472dd
Samuel Gref <samgref@gmail.com>
2021-04-14 13:04:46 -0400

    Impliment keys in Lists

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 const ImageList = props => {
+
   const images = props.images.map(image => {
-    return <img src={image.urls.regular} />;
+
+    return <img alt={image.description} key={image.id} src={image.urls.regular} />;
+
   });
 
   return <div>{images}</div>;
